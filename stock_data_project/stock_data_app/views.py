ad910912f68e41885277a2618d7f0755c8227f77
Oscar Wignall <55146481+owignall@users.noreply.github.com>
2020-05-02 22:16:09 +0100

    Version 1

--- stock_data_project/stock_data_app/views.py
+++ stock_data_project/stock_data_app/views.py
@@ -5,24 +5,21 @@ from . import fetch
 # Create your views here.
 def home(request):
 	if request.method == "POST":
-		form = Symbol(request.POST)
-		if form.is_valid():
-			code = (form.cleaned_data["name"]).upper()
-			data = fetch.data_fetch(code)
-			# dictionary = {
-			# 	"form":form, 
-			# 	"run":True,
-			# 	"close":'Test',
-			# 	"high":'Test',
-			# 	'low':'Test',
-			# 	'open':'Test',
-			# 	'previous_close':'Test',
-			# 	'code':code
-			# }
-			data.update({'form' : form})
-			data.update({'run' : True})
-			data.update({'code' : code})
-			return render(request, 'stock_data_app/home.html', data)
+		try:
+			form = Symbol(request.POST)
+			if form.is_valid():
+				code = (form.cleaned_data["name"]).upper()
+				data = fetch.data_fetch(code)
+				data.update({'form' : form})
+				data.update({'run' : True})
+				data.update({'code' : code})
+				return render(request, 'stock_data_app/home.html', data)
+		except Exception as e:
+			dictionary = {
+				"form":form, 
+				"run":False
+			}
+			return render(request, 'stock_data_app/home.html', dictionary)
 	else:
 		form = Symbol()
 		dictionary = {
